ecbe9ad56ddf03f9d630c7131ad370b391326dd5
Mayankk7 <mayank26032@gmail.com>
2022-01-17 03:08:15 +0530

    deployement bugs fixed

--- server.js
+++ server.js
@@ -19,6 +19,10 @@ const port = process.env.PORT || 5000;
 
 if ( process.env.NODE_ENV == "production" ){
     app.use(express.static("client/build"));
+
+    app.get('*', (request, response) => {
+        response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    });
 }
 
 
