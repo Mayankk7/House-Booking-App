497eeebca6b3d400a3d9c57ed56a300fde6ef6d8
mayankk7 <mayank26032@gmail.com>
2023-01-23 15:37:58 +0530

    production

--- server.js
+++ server.js
@@ -21,13 +21,11 @@ app.use("/api/bookings",bookingRoute)
 const port = process.env.PORT || 5000;
 
 
-if ( process.env.NODE_ENV == "production" ){
-    app.use(express.static("client/build"));
+    app.use(express.static(path.join(__dirname, "client/build")));
 
     app.get('*', (request, response) => {
         response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
     });
-}
 
 
 app.listen(port, ()=>{
